b0364a5e9ba5e213bcaaec5efef805ea4c1ca2e0
Saylar <Saylar@DESKTOP-CGG4BU8>
2020-03-25 19:03:26 +0100

    actividad dia 24, dia 2 fin

--- src/com/ipartek/formacion/clases/clinica/ClinicApp.java
+++ src/com/ipartek/formacion/clases/clinica/ClinicApp.java
@@ -56,50 +56,50 @@ public class ClinicApp {
 							System.out.println("Insert the name of the animal:");
 							r.setName(keyboard.nextLine());
 
-//							System.out.println("Insert the species of the animal:");
-//							r.setSpecies(keyboard.nextLine());
-//
-//							System.out.println("Insert the race of the animal:");
-//							r.setRace(keyboard.nextLine());
-//
-//							System.out.println("Insert the age of the animal:");
-//							r.setAge(Integer.parseInt(keyboard.nextLine()));
-//
-//							do {
-//								System.out.println("Insert the date (Format DD/MM/YYYY) the animal arrived:");
-//								r.setDate(keyboard.nextLine());
-//
-//								try {
-//
-//									date = LocalDate.parse(r.getDate(), dateFormatter);
-//									today = LocalDate.now();
-//
-//									if(date.isAfter(today)) {
-//
-//										System.out.println("The date must be today or older.");
-//										check = false;
-//
-//									} else {
-//
-//										check = true;
-//									}
-//								}
-//
-//								catch (DateTimeParseException e) {
-//									System.out.println("Invalid date format. DD/MM/YYYY");
-//								}
-//
-//							} while (check == false);
-//							
-//							
-//							System.out.println("Insert the reason for the visit to the clinic:");
-//							r.setReason(keyboard.nextLine());
-//
-//							System.out.println("Insert the diagnosis:");
-//							r.setDiagnosis(keyboard.nextLine());
-//
-//							System.out.println("Insert the treatment:");
-//							r.setTreatment(keyboard.nextLine());
+							System.out.println("Insert the species of the animal:");
+							r.setSpecies(keyboard.nextLine());
+
+							System.out.println("Insert the race of the animal:");
+							r.setRace(keyboard.nextLine());
+
+							System.out.println("Insert the age of the animal:");
+							r.setAge(Integer.parseInt(keyboard.nextLine()));
+
+							do {
+								System.out.println("Insert the date (Format DD/MM/YYYY) the animal arrived:");
+								r.setDate(keyboard.nextLine());
+
+								try {
+
+									date = LocalDate.parse(r.getDate(), dateFormatter);
+									today = LocalDate.now();
+
+									if(date.isAfter(today)) {
+
+										System.out.println("The date must be today or older.");
+										check = false;
+
+									} else {
+
+										check = true;
+									}
+								}
+
+								catch (DateTimeParseException e) {
+									System.out.println("Invalid date format. DD/MM/YYYY");
+								}
+
+							} while (check == false);
+							
+							
+							System.out.println("Insert the reason for the visit to the clinic:");
+							r.setReason(keyboard.nextLine());
+
+							System.out.println("Insert the diagnosis:");
+							r.setDiagnosis(keyboard.nextLine());
+
+							System.out.println("Insert the treatment:");
+							r.setTreatment(keyboard.nextLine());
 
 							
 							do {
